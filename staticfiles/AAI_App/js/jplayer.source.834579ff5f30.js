c2cffa6ef1aa4d4c33847fe3091c66108df23e33
theblindguidedog <theblindguidedog@gmail.com>
2022-03-20 19:35:19 +1100

    Merge branch 'master' of github.com:theblindguidedog/ArtAgainstIgnorance

---
+++ staticfiles/AAI_App/js/jplayer.source.834579ff5f30.js
@@ -0,0 +1,215 @@
+
+//<![CDATA[
+$(document).ready(function() {
+	
+	// homepage audio-player
+	
+	new jPlayerPlaylist({
+		jPlayer: "#tr-player",
+		cssSelectorAncestor: "#tr-player-wrapper"
+	}, [
+		{
+			title:"Cro Magnon Man",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+		},
+		{
+			title:"Stirring of a Fool",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-08-Stirring-of-a-fool.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-08-Stirring-of-a-fool.ogg"
+		},
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+		{
+			title:"Cyber Sonnet",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+		},
+		{
+			title:"Tempered Song",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
+		},
+		{
+			title:"Hidden",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
+		},
+		{
+			title:"Lentement",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
+		},
+		{
+			title:"Lismore",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-04-Lismore.ogg"
+		},
+		{
+			title:"The Separation",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-05-The-separation.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-05-The-separation.ogg"
+		},
+		{
+			title:"Beside Me",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-06-Beside-me.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-06-Beside-me.ogg"
+		},
+		{
+			title:"Bubble",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+		},
+		
+		{
+			title:"Partir",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-09-Partir.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-09-Partir.ogg"
+		},
+		{
+			title:"Thin Ice",
+			mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+		}
+	], {
+		swfPath: "js/jquery.jplayer.swf",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true,
+		remainingDuration: true,
+	});
+	
+
+});
+
+
+//<![CDATA[
+    $(document).ready(function(){
+        // ========= jPlayer Album Detail Page ==========
+        var myPlaylist = new jPlayerPlaylist({
+            jPlayer: "#jplayer",
+            cssSelectorAncestor: "#jp-container"
+        }, [
+            {
+                artist: "Blatant Blathiest", // the artist name
+                title:"<span>01.</span> Dear Mother Nature", // track title
+                mp3:"static/AAI_App/audio/m4a/DearMotherNature.m4a",// m4a path
+                oga:"",// oga path
+                duration: '2:22'// duration time in playlist
+            },
+            // extra_songs.js was here
+            
+        ], {
+            swfPath: "assets/jplayer/jplayer",
+            supplied: "oga, mp3",
+            wmode: "window",
+            useStateClassSkin: true,
+            autoBlur: false,
+            smoothPlayBar: true,
+            keyEnabled: true,
+            size: {width: "100%"}
+        });
+        // Show The Current Track !!
+        $("#jplayer").on($.jPlayer.event.ready, function (event) {
+            var current = myPlaylist.current;
+            var playlist = myPlaylist.playlist;       
+            $.each(playlist, function (index, obj) {
+                if (index == current) {
+                    $("#playing").html("<span class='artist-name'>" + obj.artist + "</span>" + "<br>" + "<span class='track-name'>" + obj.title + "</span>");
+                }
+            });
+        });
+        $("#jplayer").on($.jPlayer.event.play, function (event) {
+            var current = myPlaylist.current;
+            var playlist = myPlaylist.playlist;       
+            $.each(playlist, function (index, obj) {
+                if (index == current) {
+                    $("#playing").html("<span class='artist-name'>" + obj.artist + "</span>" + "<br>" + "<span class='track-name'>" + obj.title + "</span>");
+                }
+            });
+        });
+
+    });//]]>
+
+
+
+//<![CDATA[
+    $(document).ready(function(){
+    	// ========= About Page ==========
+        $("#music").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+                    oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
+                });
+            },
+           
+        });
+
+        $("#music1").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+                    oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+                });
+            },
+            cssSelectorAncestor: '#music-1'
+        });
+
+        $("#music2").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    mp3:"http://www.jplayer.org/audio/mp3/Miaow-09-Partir.mp3",
+                    oga:"http://www.jplayer.org/audio/ogg/Miaow-09-Partir.ogg"
+                });
+            },
+            cssSelectorAncestor: '#music-2'
+        });
+
+        $("#music3").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+                    oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
+                });
+            },
+            cssSelectorAncestor: '#music-3'
+        });
+
+        $("#music4").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+                    oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+                });
+            },
+            cssSelectorAncestor: '#music-4'
+        });
+
+        $("#music5").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    mp3:"http://www.jplayer.org/audio/mp3/Miaow-08-Stirring-of-a-fool.mp3",
+                    oga:"http://www.jplayer.org/audio/ogg/Miaow-08-Stirring-of-a-fool.ogg"
+                });
+            },
+            cssSelectorAncestor: '#music-5'
+        });
+
+        $("#music6").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+                    oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
+                });
+            },
+            cssSelectorAncestor: '#music-6'
+        });
+    });//]]> 
+  
---
+++ staticfiles/AAI_App/js/main.js
@@ -0,0 +1,317 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // -------------------------------------------------------------
+    //  backstretch slide for main body
+    // -------------------------------------------------------------
+
+    // js_parts/backstretch.js was here instead of the function below
+    (function () {  
+
+        $(".body-base").backstretch([ "static/AAI_App/images/slider/slider4.jpg","static/AAI_App/images/slider/slider5.jpg"], {fade: 750,duration: 5000,});  
+
+    }());
+
+
+    // -------------------------------------------------------------
+    //  Cubeportfolio
+    // -------------------------------------------------------------
+
+    
+    (function () {
+
+        $('#portfolio-item').cubeportfolio({
+            filters: '#portfolio-menu',
+            loadMore: '#portfolio-menu',
+            animationType: '3dflip',
+            gapVertical: 20,
+            gapHorizontal: 20,
+            gridAdjustment: 'responsive',
+            mediaQueries: [{
+                width: 992,
+                cols: 4
+            }, {
+                width: 480,
+                cols: 3
+            }, {
+                width: 320,
+                cols: 2
+            }, {
+                width: 0,
+                cols: 1
+            }],                       
+
+        });            
+
+    }());    
+
+
+    // -------------------------------------------------------------
+    //  Owl Carousel
+    // -------------------------------------------------------------
+
+	(function() {
+
+		$(".clients-slider").owlCarousel({
+			items:4,
+			nav:true,
+			autoplay:true,
+			dots:false,
+			navText: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    slideBy:1
+                },
+                480: {
+                    items: 2,
+                    slideBy:1
+                },
+                768: {
+                    items: 3,
+                    slideBy:1
+                },
+                991: {
+                    items: 4,
+                    slideBy:1
+                },
+            }  
+		});
+
+		$(".twitter-slider").owlCarousel({
+			items:1,
+			nav:true,
+			autoplay:true,
+			dots:false,
+			navText:false,                                
+   
+		});
+
+		$(".products-slider").owlCarousel({
+			items:1,
+			nav:true,
+			autoplay:true,
+			dots:false,
+			navText: [
+    			"<i class='fa fa-angle-left'></i>",
+    			"<i class='fa fa-angle-right'></i>"
+			],                       
+   
+		}); 
+
+		$(".slider").owlCarousel({
+			items:3,
+			nav:true,
+			autoplay:true,
+			dots:false,
+			navText: [
+    			"<i class='fa fa-angle-left'></i>",
+    			"<i class='fa fa-angle-right'></i>"
+			],
+            responsive: {
+                0: {
+                    items: 1,
+                    slideBy:1
+                },
+                480: {
+                    items: 1,
+                    slideBy:1
+                },
+                768: {
+                    items: 2,
+                    slideBy:1
+                },
+                991: {
+                    items:3,
+                    slideBy:1
+                },
+            }  
+		}); 
+
+		$(".albums-slider").owlCarousel({
+			items:4,
+			nav:true,
+			autoplay:true,
+			dots:false,
+			navText: [
+    			"<i class='fa fa-angle-left'></i>",
+    			"<i class='fa fa-angle-right'></i>"
+			],
+            responsive: {
+                0: {
+                    items: 1,
+                    slideBy:1
+                },
+                480: {
+                    items: 2,
+                    slideBy:1
+                },
+                768: {
+                    items: 3,
+                    slideBy:1
+                },
+                991: {
+                    items:4,
+                    slideBy:1
+                },
+            }   
+		});  
+	}());      
+         
+         
+
+	 
+	// -------------------------------------------------------------
+    //  Video Play Icon
+    // -------------------------------------------------------------
+	
+	(function () {
+
+        $('.jp-play.play-icon').on('click', function() {
+           $(this).addClass('icon-hide');   
+        });
+		
+        $('button.jp-play').on('click', function() {
+            $('.play-icon.play-icon').removeClass('icon-hide');
+        });
+
+    }());
+
+    // -------------------------------------------------------------
+    //  MagnificPopup
+    // -------------------------------------------------------------
+
+    (function() {
+        
+        $('.video-link').magnificPopup({type:'iframe'});
+
+    }()); 
+
+    (function() {
+        
+        $('.showcase-icons.popup a').magnificPopup({
+          type: 'image',
+          gallery:{
+            enabled:true
+          }
+        });
+
+    }());
+
+    // -------------------------------------------------------------
+    // Rating Bar
+    // -------------------------------------------------------------
+ 
+	$('.rating-bar').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$.each($('div.progress-bar'),function(){
+				$(this).css('width', $(this).attr('aria-valuenow')+'%');
+			});
+			$(this).off('inview');
+		}
+	});
+
+    // -------------------------------------------------------------
+    //  language Select
+    // -------------------------------------------------------------
+
+   (function() {
+
+        $('.category-dropdown').on('click', '.category-change a', function(ev) {
+            if ("#" === $(this).attr('href')) {
+                ev.preventDefault();
+                var parent = $(this).parents('.category-dropdown');
+                parent.find('.change-text').html($(this).html());
+            }
+        });
+
+    }());    
+
+
+
+    // -------------------------------------------------------------
+    //   Google Map 
+    // -------------------------------------------------------------
+
+   (function(){
+
+        var map;
+
+        map = new GMaps({
+            el: '#gmap',
+            lat: 47.0712247,
+            lng: 2.3989918,
+            scrollwheel:false,
+            zoom: 8,
+            zoomControl : true,
+            panControl : false,
+            streetViewControl : true,
+            mapTypeControl: false,
+            overviewMapControl: false,
+            clickable: false
+        });
+
+
+        var image = 'static/AAI_App/images/map-icon.png';
+        map.addMarker({
+            lat: 47.0712247,
+            lng: 2.3989918,
+            icon: image,
+            animation: google.maps.Animation.DROP,
+            verticalAlign: 'bottom',
+            horizontalAlign: 'center',
+            backgroundColor: '#d3cfcf',
+        });
+
+
+        var styles = [ 
+
+        {
+            "featureType": "road",
+            "stylers": [
+            { "color": "#979797" }
+            ]
+        },{
+            "featureType": "water",
+            "stylers": [
+            { "color": "#aaaaaa" }
+            ]
+        },{
+            "featureType": "landscape",
+            "stylers": [
+            { "color": "#e2e2e2" }
+            ]
+        },{
+            "elementType": "labels.text.fill",
+            "stylers": [
+            { "color": "#000000" }
+            ]
+        },{
+            "featureType": "poi",
+            "stylers": [
+            { "color": "#b9b9b9" }
+            ]
+        },{
+            "elementType": "labels.text",
+            "stylers": [
+            { "saturation": 1 },
+            { "weight": 0.1 },
+            { "color": "#101010" }
+            ]
+        }
+
+        ];
+
+        map.addStyle({
+            styledMapName:"Styled Map",
+            styles: styles,
+            mapTypeId: "map_style"  
+        });
+
+        map.setStyle("map_style");
+    }());          
+
+
+}); // custom js end
+
