8c4dc9fd13f87663f3ab3ab8b517526a453db433
theblindguidedog <theblindguidedog@gmail.com>
2022-04-24 13:20:42 +1000

    Removed superfluous webpages.

--- AAI_App/views.py
+++ AAI_App/views.py
@@ -3,57 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'AAI_App/index.html', {})
 
-def about(request):
-    return render(request, 'AAI_App/about.html', {})
-
-def contact(request):
-    return render(request, 'AAI_App/contact.html', {})
-
-def test(request):
-    return render(request, 'AAI_App/test.html', {})
-
-def music(request):
-    return render(request, 'AAI_App/music.html', {})
-
-def video(request):
-    return render(request, 'AAI_App/video.html', {})
-
-def shop(request):
-    return render(request, 'AAI_App/shop.html', {})
-
-def shop_details(request):
-    return render(request, 'AAI_App/shop_details.html', {})
-
-def shop_details_liamgervaise(request):
-    return render(request, 'AAI_App/shop_details_liamgervaise.html', {})
-
-def news(request):
-    return render(request, 'AAI_App/news.html', {})
-
-def news_details(request):
-    return render(request, 'AAI_App/news_details.html', {})
-
-def news_details_v2(request):
-    return render(request, 'AAI_App/news_details_v2.html', {})
-
-def news_details_aron_ra(request):
-    return render(request, 'AAI_App/news_details_aron_ra.html', {})
-
-def news_details_lawrence_krauss(request):
-    return render(request, 'AAI_App/news_details_lawrence_krauss.html', {})
-
-def news_details_lawrence_krauss_v1(request):
-    return render(request, 'AAI_App/news_details_lawrence_krauss_v1.html', {})
-
-def news_details_richard_dawkins(request):
-    return render(request, 'AAI_App/news_details_richard_dawkins.html', {})
-
-def news_details_utopians(request):
-    return render(request, 'AAI_App/news_details_utopians.html', {})
-
-def artists(request):
-    return render(request, 'AAI_App/artists.html', {})
-
 def artist_details_briangervaise(request):
     return render(request, 'AAI_App/artist_details_briangervaise.html', {})
 
@@ -63,9 +12,6 @@ def artist_details_zacharygervaise(request):
 def artist_details_darceygervaise(request):
     return render(request, 'AAI_App/artist_details_darceygervaise.html', {})
     
-def artist_details_darceygervaise2(request):
-    return render(request, 'AAI_App/artist_details_darceygervaise2.html', {})
-
 def artist_details_wally(request):
     return render(request, 'AAI_App/artist_details_wally.html', {})
 
@@ -75,26 +21,32 @@ def artist_details_johnno(request):
 def artist_details_ganna(request):
     return render(request, 'AAI_App/artist_details_ganna.html', {})
 
-def artist_details_ganna2(request):
-    return render(request, 'AAI_App/artist_details_ganna2.html', {})
-
 def artist_details_ganna3(request):
     return render(request, 'AAI_App/artist_details_ganna3.html', {})
 
 def artist_details_nerdyrodent(request):
     return render(request, 'AAI_App/artist_details_nerdyrodent.html', {})
 
-def artist_details_nerdyrodent2(request):
-    return render(request, 'AAI_App/artist_details_nerdyrodent2.html', {})
-
 def artist_details_blatantblatheist(request):
     return render(request, 'AAI_App/artist_details_blatantblatheist.html', {})
 
-def artist_details_blatantblatheist2(request):
-    return render(request, 'AAI_App/artist_details_blatantblatheist2.html', {})
+def news_details_aron_ra(request):
+    return render(request, 'AAI_App/news_details_aron_ra.html', {})
+
+def news_details_lawrence_krauss(request):
+    return render(request, 'AAI_App/news_details_lawrence_krauss.html', {})
+
+def news_details_richard_dawkins(request):
+    return render(request, 'AAI_App/news_details_richard_dawkins.html', {})
+
+def news_details_utopians(request):
+    return render(request, 'AAI_App/news_details_utopians.html', {})
 
 def critical_thinking(request):
     return render(request, 'AAI_App/critical-thinking.html', {})
+
+def test(request):
+    return render(request, 'AAI_App/test.html', {})
 
 
 
